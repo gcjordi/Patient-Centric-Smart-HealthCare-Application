7c6abc12c5bb238502c4635a9725c1af54b9e357
Abhishek-Kathayat <abhishekkathayat25@gmail.com>
2019-12-20 17:56:07 +0530

    Added Routes

--- src/main.js
+++ src/main.js
@@ -1,10 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
 import vuetify from './plugins/vuetify';
 
+Vue.use(VueRouter)
 Vue.config.productionTip = false
+
+const routes = [
+  { path: '/test', component: }
+]
+
+const router = new VueRouter({
+  routes
+})
 
 new Vue({
   vuetify,
+  router,
   render: h => h(App)
 }).$mount('#app')
